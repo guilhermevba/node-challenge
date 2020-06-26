1ffc1d8794c198c858cfdf8586fa09e2426d2f93
Guilherme Azevedo <guilhermevba@gmail.com>
2020-06-26 14:14:17 -0300

    feat: implement test cases features

---
+++ makefile
@@ -0,0 +1,8 @@
+test:
+	npm run test
+
+test-watch:
+	npm run test-watch
+
+run:
+	npm run dev
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
+const { response } = require("express");
 
 const app = express();
 
@@ -11,23 +12,55 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.send(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const repository = {
+    id: uuid(),
+    likes: 0,
+    ...request.body,
+  }
+  repositories.push(repository)
+  return response.send(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const index = repositories.findIndex(({ id }) => request.params.id === id)
+  if (index === -1) {
+    return response.status(400).send()
+  }
+
+  const updatedRepository = {
+    ...request.body,
+    id: repositories[index].id,
+    likes: repositories[index].likes
+  }
+  repositories.splice(index, 1, updatedRepository)
+  response.send(updatedRepository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const index = repositories.findIndex(({ id }) => request.params.id === id)
+  if(index === -1) {
+    return response.status(400).send()
+  }
+  repositories.splice(index, 1)
+  response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const index = repositories.findIndex(({ id }) => request.params.id === id)
+  if (index === -1) {
+    return response.status(400).send()
+  }
+
+  const updatedRepository = {
+    ...repositories[index],
+    likes: repositories[index].likes + 1
+  }
+  repositories.splice(index, 1, updatedRepository)
+  response.send(updatedRepository)
 });
 
 module.exports = app;
